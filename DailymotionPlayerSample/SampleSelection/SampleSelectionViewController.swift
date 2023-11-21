1eeea5ea88e7b1818eb2d35cfadb703bacea47e9
Florin Dobjenschi <fdobjenschi@pentalog.com>
2023-11-21 16:04:25 +0200

    updated SDK

--- DailymotionPlayerSample/SampleSelection/SampleSelectionViewController.swift
+++ DailymotionPlayerSample/SampleSelection/SampleSelectionViewController.swift
@@ -21,7 +21,7 @@ class SampleSelectionViewController: DailymotionBaseViewController {
     
     func setupView() {
         let buildNumber = Bundle.main.object(forInfoDictionaryKey: "CFBundleShortVersionString") as? String ?? ""
-        appVersionLabel.text = "App v\(buildNumber)"
+        appVersionLabel.text = "App v\(buildNumber)_alpha"
         SDKVersionLabel.text = "SDK v\(DailymotionPlayer.sdkVersion)"
         overrideUserInterfaceStyle = .light
         title = NSLocalizedString("SampleScreenSelectionTitle", comment: "")
