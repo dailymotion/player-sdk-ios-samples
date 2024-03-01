a1a5f91024bdcc9cab471bc787cbcab700be3783
Florin Dobjenschi <fdobjenschi@pentalog.com>
2024-03-01 10:09:59 +0200

    deal with secrets

--- ci_scripts/replace_sample_secret.sh
+++ ci_scripts/replace_sample_secret.sh
@@ -1,4 +1,21 @@
-sed -i '' -e "s/<<AppCenterSecret>>/${APPCENTER_API_TOKEN}/g" ${CI_MAIN_REPO_PATH}/DailymotionPlayerSample/AppDelegate.swift
+appcenter_secret=""
+case ${{inputs.channel_type}} in
+stable)
+appcenter_secret=$APP_CENTER_SECRET_PROD
+;;
+beta)
+appcenter_secret=$APP_CENTER_SECRET_BETA
+;;
+alpha)
+appcenter_secret=$APP_CENTER_SECRET_ALPHA
+;;
+*)
+echo -n "Unknown release channel_type" ; exit 1
+;;
+esac
+echo ${#appcenter_secret}
+
+sed -i '' -e "s/<<AppCenterSecret>>/${appcenter_secret}/g" ${CI_MAIN_REPO_PATH}/DailymotionPlayerSample/AppDelegate.swift
 echo "AppDelegate secret replaced"
-sed -i '' -e "s/AppCenterSecretPLIST/appcenter-${APPCENTER_API_TOKEN}/g" ${CI_MAIN_REPO_PATH}/DailymotionPlayerSample/Info.plist
+sed -i '' -e "s/AppCenterSecretPLIST/appcenter-${appcenter_secret}/g" ${CI_MAIN_REPO_PATH}/DailymotionPlayerSample/Info.plist
 echo "Info.plist secret replaced"
